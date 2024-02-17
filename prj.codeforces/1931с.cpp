f6928fd1e6135e19e0d2c84b4768c8628b600abe
Lol <denis.kalinkin.05@gmail.com>
2024-02-17 15:41:40 +0300

    Add codeforces

---
+++ prj.codeforces/1931с.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    int t = 0;
+    std::cin >> t;
+    while (t > 0){
+        int p = 0;
+        std::cin >> p;
+        std::vector<int> arr;
+        while(p > 0){
+            int temp;
+            std::cin >> temp;
+            arr.push_back(temp);
+        }
+    }
+}
