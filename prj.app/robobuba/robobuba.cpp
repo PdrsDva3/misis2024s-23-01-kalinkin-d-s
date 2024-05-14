d2ca892b7722c7b8b3229c8a52a4b71c373e7f34
Lol <denis.kalinkin.05@gmail.com>
2024-05-14 14:02:06 +0300

    робобуба преследует меня

---
+++ prj.app/robobuba/robobuba.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Command{
+public:
+    virtual ~Command() = default;
+    virtual std::string cmdName() const = 0;
+    virtual void execute(int& index, std::vector<int>& vec) = 0;
+};
+
+
+class CmdLeft : public Command{
+public:
+    void execute(int &index, std::vector<int> &vec) override{
+        index = (index - L + vec.size()) % vec.size();
+    }
+    std::string cmdName() const override{
+        return "LEFT " + std::to_string(L);
+    }
+private:
+    int L;
+};
+
+class CmdRight : public Command{
+public:
+    void execute(int &index, std::vector<int> &vec) override{
+        index = (index + R + vec.size()) % vec.size();
+    }
+    std::string cmdName() const override{
+        return "RIGHT " + std::to_string(R);
+    }
+private:
+    int R;
+};
+
+class CmdAdd : public Command{
+public:
+    void execute(int &index, std::vector<int> &vec) override{
+        vec[index] += D;
+    }
+    std::string cmdName() const override{
+        return "ADD " + std::to_string(D);
+    }
+private:
+    int D;
+};
+
+class CmdSub : public Command{
+public:
+    void execute(int &index, std::vector<int> &vec) override{
+        vec[index] *= B;
+    }
+    std::string cmdName() const override{
+        return "ADD " + std::to_string(B);
+    }
+private:
+    int B;
+};
+
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5};
+    std::vector<Command*> cmdlist;
+
+    std::string name;
+    while (true){
+        std::getline(std::cin, name);
+
+        if (name == "^C") {
+            break;
+        }
+
+    }
+
+    return 0;
+}
